7a7eb0e9a997684a51550d88a34c625555856d36
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2021-05-11 21:08:30 +0200

    upload !

--- api/src/server.py
+++ api/src/server.py
@@ -1,20 +1,19 @@
 from flask import Flask, request, make_response
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-#app.secret_key = b"\x04\x15'bc\xf1\xfamO\xd6L\x87\x98\xb6\xc9^"
 CORS(app)
 
-@app.route('/', defaults={'path': ''}, methods=['POST', 'GET'])
-@app.route('/<path:path>', methods=['POST', 'GET', 'DELETE'])
-def main_entry_point(path):
-    method = request.method
-    request_query = request.args.copy()
-    request_body = request.json if request.json else {}
- 
-    print(method)
-    print(request_body)
-    print(request_query)
+UPLOAD_FOLDER = 'UPLOAD_FOLDER'
+
+os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    filename = file.filename
+    file.save(os.path.join('UPLOAD_FOLDER', filename))
 
     return make_response({}, 200)
 
