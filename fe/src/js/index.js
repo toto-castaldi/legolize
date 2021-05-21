22a3d8104dd68ba5c6e3bdd5b104d9b7b19034bc
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2021-05-21 19:43:21 +0200

    desktop move. mobile blocked

--- fe/src/js/index.js
+++ fe/src/js/index.js
@@ -3,6 +3,28 @@ const elementId = (pos) => `${pos.x}-${pos.y}`;
 const start = (data) => {
     let scale = 1;
 
+    let startDrag = undefined;
+    let delta = {
+        x: 0,
+        y: 0
+    }
+
+    const mousedown = (event) => {
+        event.preventDefault();
+        startDrag = {
+            x: event.x,
+            y: event.y
+        }
+    }
+
+    const mouseup = (event) => {
+        event.preventDefault();
+        delta.x += event.x - startDrag.x;
+        delta.y += event.y - startDrag.y;
+        draw();
+        startDrag = undefined;
+    }
+
     //load thumbnail
     document.getElementById("thmbnail-img").src = data.thmbnailSrc;
 
@@ -11,6 +33,8 @@ const start = (data) => {
         const element = document.createElement("div");
         element.id = elementId(palettePoint.pos);
         render.appendChild(element);
+        //element.onmousedown = mousedown;
+        //element.onmouseup = mouseup;
 
         element.classList.add(`element`, `palette-${palettePoint.colorId}`);
     }
@@ -23,8 +47,8 @@ const start = (data) => {
         const minRenderLen = (renderW > renderH ? renderH : renderW) * 0.9 * scale;
         const maxRenderElement = data.renderedInfo.w > data.renderedInfo.h ? data.renderedInfo.w : data.renderedInfo.h;
         const elementLen = minRenderLen / maxRenderElement;
-        const centerX = renderW / 2;
-        const centerY = renderH / 2;
+        const centerX = renderW / 2 + delta.x;
+        const centerY = renderH / 2 + delta.y;
         const startX = centerX - (data.renderedInfo.w / 2) * elementLen;
         const startY = centerY - (data.renderedInfo.h / 2) * elementLen;
         for (palettePoint of data.renderedInfo.points) {
@@ -38,6 +62,8 @@ const start = (data) => {
             element.style.top = Math.ceil(startY + palettePointY * elementLen) + "px";
         }
     }
+
+    const el = document.getElementById("app");
 
     //zoom
     const zoom = (event) => {
@@ -57,15 +83,16 @@ const start = (data) => {
 
         draw();
     }
-
-    const el = document.getElementById("app");
     el.onwheel = zoom;
 
+    el.onmousedown = mousedown;
+    el.onmouseup = mouseup;
 
     window.onresize = () => {
         scale = 1;
         draw();
     }
+
 
     draw();
 }
