f586e05586b1fe303ec4bd03e6b05af3c9ce937a
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2021-05-21 12:07:04 +0200

    grey !

--- fe/src/components/NavBar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <nav class="navbar sticky-top navbar-light bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand text-white" href="/" >Legolize</a>
-    </div>
-  </nav>
-</template>
